c19e091257cf5421a80398b8a193f795bdc9fb72
LuK1337 <priv.luk@gmail.com>
2021-09-06 18:38:59 +0530

    avicii: livedisplay: Uprev to 2.1
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- livedisplay/DisplayModes.h
+++ livedisplay/DisplayModes.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYMODES_H
-#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYMODES_H
+#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_1_DISPLAYMODES_H
+#define VENDOR_LINEAGE_LIVEDISPLAY_V2_1_DISPLAYMODES_H
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
@@ -25,7 +25,7 @@
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
-namespace V2_0 {
+namespace V2_1 {
 namespace implementation {
 
 using ::android::hardware::Return;
@@ -34,9 +34,15 @@ using ::android::sp;
 
 class DisplayModes : public IDisplayModes {
   public:
-    DisplayModes();
 
-    // Methods from ::vendor::lineage::livedisplay::V2_0::IDisplayModes follow.
+    DisplayModes(std::shared_ptr<V2_0::sdm::SDMController> controller);
+
+    using DisplayModeSetCallback = std::function<void()>;
+    inline void registerDisplayModeSetCallback(DisplayModeSetCallback callback) {
+        mOnDisplayModeSet = callback;
+    }
+
+    // Methods from ::vendor::lineage::livedisplay::V2_1::IDisplayModes follow.
     Return<void> getDisplayModes(getDisplayModes_cb resultCb) override;
     Return<void> getCurrentDisplayMode(getCurrentDisplayMode_cb resultCb) override;
     Return<void> getDefaultDisplayMode(getDefaultDisplayMode_cb ResultCb) override;
@@ -48,13 +54,15 @@ class DisplayModes : public IDisplayModes {
         std::string node;
     };
     static const std::map<int32_t, ModeInfo> kModeMap;
+    std::shared_ptr<V2_0::sdm::SDMController> mController;
+    int32_t mCurrentModeId;
     int32_t mDefaultModeId;
 };
 
 }  // namespace implementation
-}  // namespace V2_0
+}  // namespace V2_1
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
 
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYMODES_H
+#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_1_DISPLAYMODES_H
