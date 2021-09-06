1998ad5e325b3e7b517d2ed1e1af0a99cf5b00a7
AmolAmrit <amol.amrit03@outlook.com>
2021-09-06 23:24:04 +0530

    avicii: Use device name as product device & name
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- aosp_avicii.mk
+++ aosp_avicii.mk
@@ -42,6 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/Nord/Nord:11/RP1A.201005.001/2108131839:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=avicii \
-    PRODUCT_NAME=aosp_avicii \
+    PRODUCT_DEVICE=OnePlusNord \
+    PRODUCT_NAME=OnePlusNord \
     PRIVATE_BUILD_DESC="Nord-user 11 RP1A.201005.001 2108131839 release-keys"
