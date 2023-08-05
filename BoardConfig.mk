a6699001296d8a9942122c497a5c181b2cef7249
AmolAmrit <amol.amrit03@outlook.com>
2023-08-06 00:04:37 +0530

    avicii: Adapt for BlissRoms
    * Change overlay path and adapt overlays required for UDFPS, Charging Info, Touch Gestures and other device side stuff.
    * There might be duplication of overlays, but it is just to be on a safer side.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/bliss/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
